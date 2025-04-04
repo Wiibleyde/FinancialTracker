e0c2509cbaa1966f53157836d0b009789ec78911
Matsuel <matsuel0308@gmail.com>
2025-04-04 16:39:46 +0200

    Fix date handling in AddLineView and ExpenseDAO to default to the first of the month

---
+++ src/main/java/com/financialtracker/AddIncomeView.java
@@ -0,0 +1,49 @@
+package com.financialtracker;
+
+import com.financialtracker.db.IncomeDAO;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class AddIncomeView {
+    @FXML
+    private TextField periodField;
+    @FXML
+    private TextField salaryField;
+    @FXML
+    private TextField grantField;
+    @FXML
+    private TextField businessSalaryField;
+    @FXML
+    private TextField passiveIncomeField;
+    @FXML
+    private TextField otherField;
+
+    private IncomesController incomesController;
+
+    public void setIncomesController(IncomesController incomesController) {
+        this.incomesController = incomesController;
+    }
+
+    @FXML
+    public void addIncome() {
+        IncomeLine incomeLine = new IncomeLine();
+        String periodText = periodField.getText();
+        if (periodText.matches("\\d{4}-\\d{2}")) {
+            periodText += "-01"; // Ajouter le jour par défaut
+        }
+        incomeLine.setPeriod(periodText);
+        incomeLine.setSalary(Float.parseFloat(salaryField.getText()));
+        incomeLine.setGrant(Float.parseFloat(grantField.getText()));
+        incomeLine.setBusinessSalary(Float.parseFloat(businessSalaryField.getText()));
+        incomeLine.setPassiveIncome(Float.parseFloat(passiveIncomeField.getText()));
+        incomeLine.setOther(Float.parseFloat(otherField.getText()));
+
+        incomesController.addIncome(incomeLine);
+
+        IncomeDAO.insertIncome(incomeLine);
+
+        Stage stage = (Stage) periodField.getScene().getWindow();
+        stage.close();
+    }
+}
--- src/main/java/com/financialtracker/IncomesController.java
+++ src/main/java/com/financialtracker/IncomesController.java
@@ -4,7 +4,14 @@ import com.financialtracker.db.IncomeDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class IncomesController {
     @FXML
@@ -16,5 +23,23 @@ public class IncomesController {
     public void initialize() {
         incomeData.addAll(IncomeDAO.getIncomes());
         tableView.setItems(incomeData);
+    }
+
+    public void addIncome(IncomeLine incomeLine) {
+        incomeData.add(incomeLine);
+    }
+
+    @FXML
+    public void openAddIncomeView() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("add-income-view.fxml"));
+        Parent parent = fxmlLoader.load();
+
+        AddIncomeView controller = fxmlLoader.getController();
+        controller.setIncomesController(this);
+
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(new Scene(parent));
+        stage.showAndWait();
     }
 }
